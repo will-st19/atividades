9fb6dafa4ce18d637650771ab07469ee00067bd5
will-st19 <94554645+will-st19@users.noreply.github.com>
2023-02-20 16:04:19 -0300

    V3 expressoes.py

--- expressoes.py
+++ expressoes.py
@@ -1,61 +1,42 @@
 import random
 
+OPERATORS = ["+", "-", "*", ":"]
+
+
 def generate_expression():
-    """
-    Gera uma expressão numérica aleatória com parênteses, colchetes e chaves,
-    com resultados inteiros.
-    """
-    operators = ['+', '-', '*', '/']
-    return generate_expression_rec(0, operators)
+    # Definindo a profundidade da expressão (quantidade de níveis de aninhamento)
+    depth = random.randint(2, 4)
 
-def generate_expression_rec(curr_depth, operators):
-    """
-    Função auxiliar para gerar a expressão numérica aleatória com parênteses, 
-    colchetes e chaves, com resultados inteiros.
-    """
-    expression = ''
-    if curr_depth == 0:
-        # primeiro nível da expressão
-        bracket_types = ['(', '{', '[']
-        expression += bracket_types[random.randint(0, len(bracket_types)-1)]
-        expression += generate_expression_rec(curr_depth+1, operators)
-        expression += bracket_types[random.randint(0, len(bracket_types)-1)]
-    elif curr_depth == 3:
-        # último nível da expressão
-        expression += str(random.randint(0, 10))
-        expression += curr_bracket(curr_depth)
-    else:
-        # outros níveis da expressão
-        if random.random() < 0.5:
-            # escolhe aleatoriamente um parêntese, colchete ou chave 
-            expression += curr_bracket(curr_depth)
-            expression += generate_expression_rec(curr_depth+1, operators)
-            expression += curr_bracket(curr_depth)
-        else:
-            # escolhe aleatoriamente um operador
-            expression += operators[random.randint(0, len(operators)-1)]
-            expression += generate_expression_rec(curr_depth, operators)
-            expression += operators[random.randint(0, len(operators)-1)]
-    return expression
+    # Lista para armazenar a expressão
+    expression = []
 
-def curr_bracket(depth):
-    """
-    Retorna o símbolo de parêntese, colchete ou chave correspondente
-    ao nível atual da expressão.
-    """
-    if depth == 1:
-        return '('
-    elif depth == 2:
-        return '{'
-    elif depth == 3:
-        return '['
-    elif depth == 4:
-        return ']'
-    elif depth == 5:
-        return '}'
-    elif depth == 6:
-        return ')'
+    # Gerando o primeiro número
+    expression.append(str(random.randint(1, 50)))
 
-# Teste: gera 10 expressões aleatórias
+    # Gerando os próximos números e operadores
+    for i in range(depth):
+        expression.append(random.choice(OPERATORS))
+        expression.append(random.choice(["(", "[", "{"]))
+        expression.append(str(random.randint(1, 50)))
+
+    # Fechando os parênteses, colchetes e chaves
+    for i in range(depth):
+        expression.append(random.choice([")", "]", "}"]))
+
+    # Juntando todos os elementos da lista em uma única string
+    expression_string = "".join(expression)
+
+    # Avaliando a expressão
+    result = eval(expression_string)
+
+    # Se o resultado não for um número inteiro, gera uma nova expressão
+    if not isinstance(result, int):
+        return generate_expression()
+
+    # Retornando a expressão como string
+    return expression_string
+
+
+# Gerando 10 expressões
 for i in range(10):
     print(generate_expression())
